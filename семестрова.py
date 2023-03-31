cd37f4c06300a9a0f1286ff12a9d8c7c43bdb1ea
theAndrew <128826609+theAndrew2808@users.noreply.github.com>
2023-03-31 17:53:23 +0300

    beta realease 1.0
    Full classic game
    Board size = 3*3
    And a lot of more!

--- семестрова.py
+++ семестрова.py
@@ -6,52 +6,80 @@ root.title('x   o')
 
 root.geometry('927x800')
 
-player_turn = "X"
-player_trn = 0
+player_turn = 'X'
+
+
+def change_turn():
+    global player_turn
+
+    if player_turn == 'X':
+        player_turn = '0'
+    elif player_turn == '0':
+        player_turn = 'X'
+    show_turn(player_turn)
 
 
 def b1_config():
     b1.config(text=player_turn)
     board[0][0] = player_turn
+    change_turn()
+    check_win(board)
+
 
 def b2_config():
     b2.config(text=player_turn)
     board[0][1] = player_turn
+    change_turn()
+    check_win(board)
 
 
 def b3_config():
     b3.config(text=player_turn)
     board[0][2] = player_turn
+    change_turn()
+    check_win(board)
 
 
 def b4_config():
     b4.config(text=player_turn)
     board[1][0] = player_turn
+    change_turn()
+    check_win(board)
 
 
 def b5_config():
     b5.config(text=player_turn)
     board[1][1] = player_turn
+    change_turn()
+    check_win(board)
 
 
 def b6_config():
     b6.config(text=player_turn)
     board[1][2] = player_turn
+    change_turn()
+    check_win(board)
 
 
 def b7_config():
     b7.config(text=player_turn)
     board[2][0] = player_turn
+    change_turn()
+    check_win(board)
 
 
 def b8_config():
     b8.config(text=player_turn)
     board[2][1] = player_turn
+    change_turn()
+    check_win(board)
 
 
 def b9_config():
     b9.config(text=player_turn)
     board[2][2] = player_turn
+    change_turn()
+    check_win(board)
 
 
 def b_quit():
@@ -61,17 +89,35 @@ def b_quit():
 def cyan_text(text):
     print(Fore.CYAN + text + Fore.RESET)
 
-def checker():
-    pass
+
+def show_turn(player_turn):
+    label.config(text=player_turn)
+
+
+def check_win(board):
+    for i in range(3):
+        'strochki'
+        if board[i][0] == board[i][1] == board[i][2] and board[i][0] != '':
+            show_winner(board[i][0])
+
+        'stolbiki'
+        if board[0][i] == board[1][i] == board[2][i] and board[0][i] != '':
+            show_winner(board[0][i])
+
+    'diagonal'
+    if board[0][0] == board[1][1] == board[2][2] and board[0][0] != '':
+        show_winner(board[0][0])
+
+    if board[0][2] == board[1][1] == board[2][0] and board[0][2] != '':
+        show_winner(board[0][2])
 
 
 
 
 
-
-
-
-
+def show_winner(winner):
+    text = f'{winner} won'
+    label.config(text=text)
 
 
 board = [
@@ -101,7 +147,8 @@ b9 = tkinter.Button(root, text="", padx=100, pady=100, command=b9_config, font="
 b10 = tkinter.Button(root, text="Quit!", padx=100, pady=100, command=b_quit, font="helvetica 14",
                      foreground="#004D40", background="#B2DFDB", )
 
-label = tkinter.Label(text=player_turn)
+label = tkinter.Label(text=player_turn, foreground="#004D40",
+                      background="#B2DFDB")
 label.grid(row=2, column=4)
 
 b1.grid(row=1, column=0)
@@ -115,11 +162,14 @@ b8.grid(row=3, column=1)
 b9.grid(row=3, column=2)
 b10.grid(row=3, column=4)
 
-start_game = input("Do you want to start the game?  (Y/N) ").capitalize()
+label = tkinter.Label(text="")
+label.grid(row=2, column=4)
+
+cyan_text("Do you want to start the game?  (Y/N) ")
+start_game = input().capitalize()
 if start_game == "Y":
     root.mainloop()
 elif start_game == "N":
     cyan_text("Goodbye, see you next time")
 
-# hello world
 print(board)
